d92f9cbb1d2a6ac70afd6cc58b9b845d14bbe671
dcshoecomp <16295807+dcshoecomp@users.noreply.github.com>
2023-09-12 21:45:45 -0400

    Add files via upload

--- removeOldDeconzAPIkeys.ps1
+++ removeOldDeconzAPIkeys.ps1
@@ -1,12 +1,19 @@
+<#
+DeConz/Phoscon automated cleanup of unused API keys.
+#>
+
 $deconzIP = "<IP>"
 $apiKey = "<key>"
 
-$wl = (Invoke-WebRequest -uri "http://$deconzIP/api/$apikey/config" -UseBasicParsing).content
-$jwl = $wl | convertfrom-json -Depth 999
-foreach($key in ($jwl.whitelist | get-member -MemberType NoteProperty ).name){
-    if($jwl.whitelist.$key."last use date" -lt (get-date).adddays(-7)){
+$webcontent = (Invoke-WebRequest -uri "http://$deconzIP/api/$apikey/config" -UseBasicParsing).content
+$json = $webcontent | convertfrom-json -Depth 10
+foreach($key in ($json.whitelist | get-member -MemberType NoteProperty ).name){
+    if($json.whitelist.$key."last use date" -lt (get-date).adddays(-7)){
         Invoke-WebRequest -uri "http://$deconzIP/api/$apikey/config/whitelist/$key" -UseBasicParsing -Method Delete
     }
 }
+
+#finally delete the api key used to delete api keys :)
+Invoke-WebRequest -uri "http://$deconzIP/api/$apikey/config/whitelist/$apikey" -UseBasicParsing -Method Delete
 
 
